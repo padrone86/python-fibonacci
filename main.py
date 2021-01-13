ca55ccdd1b20ba72701f4814f2cd7be06cd86e98
Shinpei Aoki <shi.aoki@opt.ne.jp>
2021-01-13 22:39:23 +0900

    functoolsによるメモ化

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
-def fibonacci(i: int, memo: dict = {}) -> int:
+from functools import lru_cache
+
+
+@lru_cache(maxsize=None)
+def fibonacci(i: int) -> int:
     if i == 1:
         return 0
     elif i == 2:
         return 1
-    elif i in memo:
-        return memo[i]
     elif i >= 3:
-        memo[i] = fibonacci(i - 2) + fibonacci(i - 1)
-        return memo[i]
+        return fibonacci(i - 2) + fibonacci(i - 1)
     else:
         raise Exception(f"不正な数です: {i}")
 
