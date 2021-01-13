3074b9109e2dce45b4745933786c8584622fc975
Shinpei Aoki <shi.aoki@opt.ne.jp>
2021-01-13 22:17:20 +0900

    素朴な初期実装

---
+++ main.py
@@ -0,0 +1,16 @@
+def fibonacci(i: int) -> int:
+    if i == 1:
+        return 0
+    elif i == 2:
+        return 1
+    elif i >= 3:
+        return fibonacci(i - 2) + fibonacci(i - 1)
+    else:
+        raise Exception(f"不正な数です: {i}")
+
+
+if __name__ == "__main__":
+    input_number = input("数値を入力: ")
+    for i in range(1, int(input_number)):
+        print(str(fibonacci(i)), end=" ")
+    print()
