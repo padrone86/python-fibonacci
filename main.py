6363e1479e10111e61ba8865c4d52127a7a40ed8
Shinpei Aoki <shi.aoki@opt.ne.jp>
2021-01-13 22:34:54 +0900

    メモ利用による高速化

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
-def fibonacci(i: int) -> int:
+def fibonacci(i: int, memo: dict = {}) -> int:
     if i == 1:
         return 0
     elif i == 2:
         return 1
+    elif i in memo:
+        return memo[i]
     elif i >= 3:
-        return fibonacci(i - 2) + fibonacci(i - 1)
+        memo[i] = fibonacci(i - 2) + fibonacci(i - 1)
+        return memo[i]
     else:
         raise Exception(f"不正な数です: {i}")
 
